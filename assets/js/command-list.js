e49d6ed6a6db09c00cfe34e458bd349e8a7bf17d
LeAndre <187464993+lecanact@users.noreply.github.com>
2025-12-06 16:15:54 -0500

    add description and command name

--- assets/js/command-list.js
+++ assets/js/command-list.js
@@ -11,9 +11,18 @@ async function FetchCommands() {
       console.log(cmd.name);
       console.log(cmd.description);
 
+      if (cmd.type === "slash_command") {
+        cmd.type = "Slash"
+        cmd.name = "/" + cmd.name
+      } 
+      if (cmd.type === "context_menu_command") {
+        cmd.type = "Menu"
+        cmd.name = "Right Click User/Message To Use " + cmd.name
+      }
+
       cmd_clone.querySelector("#command-name").textContent = cmd.name;
-      cmd_clone.querySelector("#command-description").textContent =
-        cmd.description;
+      cmd_clone.querySelector("#command-description").textContent = cmd.description;
+      cmd_clone.querySelector("#cmmand-type").textContent = cmd.type;
 
       cmd_clone.hidden = false;
       command_container.appendChild(cmd_clone);
