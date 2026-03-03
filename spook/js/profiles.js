f87ed22c1007ffc24dcdb9eb6cd62e516f915a01
TemporarilySpooky <lc@spook.bio>
2026-03-03 10:43:02 -0500

    Delete spook/js/profiles.js
    Co-authored-by: LeAndre <lcjunior1220@gmail.com>
    Co-authored-by: Our-Websites <lcjunior1220@spook.bio>

--- spook/js/profiles.js
+++
@@ -1,5 +0,0 @@
-{% for username, profile in profiles %}
-        document.getElementById("{{username}}").addEventListener("click", function () {
-            window.location.href = "/@{{username}}";
-        });
-        {% endfor %}
